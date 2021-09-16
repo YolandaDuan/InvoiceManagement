103c4cc21cdb8da5e0e3c188d9fc34e1ac485bbc
Yolanda Duan <malmsduan@gmail.com>
2021-09-16 17:49:52 +0200

    add invoice form

---
+++ imports/ui/InvoiceForm.jsx
@@ -0,0 +1,34 @@
+import React, { useState} from 'react';
+import { InvoicesCollection } from '../api/InvoicesCollection';
+
+export const InvoiceForm = () => {
+    const [text, setText] = useState("");
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if(!text) return;
+
+        InvoicesCollection.insert({
+            text: text.trim(),
+            createdAt: new Date()
+        });
+
+        setText("");
+    };
+
+    return (
+        <form className="invoice-form" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="add new invoices"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+
+            <button type="submit">
+                Add Invoice
+            </button>
+        </form>
+    );
+};
