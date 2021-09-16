115a3b393f0d7e82228e3317ec4585b5c7fbbebc
Yolanda Duan <malmsduan@gmail.com>
2021-09-16 15:19:13 +0200

    connect to MongoDB and set up Invoices Collection

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import { InvoicesCollection } from '../api/InvoicesCollection';
 import { Invoice } from './Invoice'
 
-const invoices = [
-  {_id: 1, text: 'first invoice'},
-  {_id: 2, text: 'second invoice'},
-  {_id: 3, text: 'third invoice'},
-];
+// const invoices = [
+//   {_id: 1, text: 'first invoice'},
+//   {_id: 2, text: 'second invoice'},
+//   {_id: 3, text: 'third invoice'},
+// ];
 
-export const App = () => (
-  <div>
-    <h1>Invoice Management</h1>
+export const App = () => {
+  const invoices = useTracker(() => InvoicesCollection.find({}).fetch());
 
-    <ul>
-      { invoices.map(invoice => <Invoice key={invoice._id} invoice={ invoice }/>) }
-    </ul>
+  return(
+    <div>
+      <h1>test Invoice Management</h1>
+  
+      <ul>
+        { invoices.map(invoice => <Invoice key={invoice._id} invoice={ invoice }/>) }
+      </ul>
+  
+    </div>
+  );
+}
 
-  </div>
-);
+
+
--- server/main.js
+++ server/main.js
@@ -1,31 +1,15 @@
 import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
+import { InvoicesCollection } from '/imports/api/InvoicesCollection';
 
-function insertLink({ title, url }) {
-  LinksCollection.insert({title, url, createdAt: new Date()});
-}
+const insertInvoice = invoiceText => InvoicesCollection.insert({text: invoiceText});
 
 Meteor.startup(() => {
   // If the Links collection is empty, add some data.
-  if (LinksCollection.find().count() === 0) {
-    insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app'
-    });
-
-    insertLink({
-      title: 'Follow the Guide',
-      url: 'http://guide.meteor.com'
-    });
-
-    insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com'
-    });
-
-    insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com'
-    });
+  if (InvoicesCollection.find().count() === 0) {
+    [
+      'invoice new',
+      'invoice 2',
+      'invoice 3'
+    ].forEach(insertInvoice)
   }
 });
