a1cc1cfdfffe42c2fcea4f7b557f97262527802b
Yolanda Duan <malmsduan@gmail.com>
2021-09-19 01:22:06 +0200

    add login form

---
+++ imports/ui/LoginForm.jsx
@@ -0,0 +1,37 @@
+import { Meteor } from "meteor/meteor";
+import React, { useState } from "react";
+
+export const LoginForm = () => {
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const submit = e => {
+        e.preventDefault();
+
+        Meteor.loginWithPassword(username, password);
+    };
+
+    return (
+        <form onSubmit={submit} className="login-form">
+            <label htmlFor="username">UserName</label>
+            <input 
+                type="text"
+                placeholder="UserName"
+                name="username"
+                required
+                onChange={e => setUserName(e.target.value)}
+            />
+
+            <label htmlFor="password">Password</label>
+            <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                required
+                onChange={e => setPassword(e.target.value)}
+            />
+
+            <button type="submit">Log In</button>
+        </form>
+    );
+};
