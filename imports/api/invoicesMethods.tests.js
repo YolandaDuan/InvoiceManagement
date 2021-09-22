569d7a38cb5c94a4615500e0379863ebc78ef1e0
Yolanda Duan <malmsduan@gmail.com>
2021-09-22 19:06:22 +0200

    fist test script runing on framework Mocha

---
+++ imports/api/invoicesMethods.tests.js
@@ -0,0 +1,9 @@
+import { Meteor } from "meteor/meteor";
+
+if (Meteor.isServer) {
+    describe('Invoices', () => {
+        describe('methods', () => {
+            it('can delete owned invoie', () => {});
+        });
+    });
+}
